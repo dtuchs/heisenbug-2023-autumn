2ef0e58095cb20505216b390d0324cb827a31362
dtuchs <dtuchs@gmail.com>
2023-09-27 02:22:09 +0600

    handle SQLIntegrityConstraintViolationException

---
+++ rococo-api/src/main/java/guru/qa/rococo/model/ErrorJson.java
@@ -0,0 +1,11 @@
+package guru.qa.rococo.model;
+
+import jakarta.annotation.Nonnull;
+
+import java.util.Date;
+import java.util.List;
+
+public record ErrorJson (@Nonnull Date timestamp,
+                         int status,
+                         @Nonnull List<String> errors){
+}
---
+++ rococo-api/src/main/java/guru/qa/rococo/service/RestExceptionHandler.java
@@ -0,0 +1,52 @@
+package guru.qa.rococo.service;
+
+import guru.qa.rococo.model.ErrorJson;
+import jakarta.annotation.Nonnull;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.sql.SQLIntegrityConstraintViolationException;
+import java.util.Date;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@RestControllerAdvice
+public class RestExceptionHandler extends ResponseEntityExceptionHandler {
+
+  @Override
+  protected @Nonnull ResponseEntity<Object> handleMethodArgumentNotValid(@Nonnull MethodArgumentNotValidException ex,
+                                                                         @Nonnull HttpHeaders headers,
+                                                                         @Nonnull HttpStatusCode status,
+                                                                         @Nonnull WebRequest request) {
+    return new ResponseEntity<>(new ErrorJson(
+        new Date(),
+        status.value(),
+        ex.getBindingResult()
+            .getFieldErrors()
+            .stream()
+            .map(DefaultMessageSourceResolvable::getDefaultMessage)
+            .collect(Collectors.toList())
+    ), headers, status);
+  }
+
+  @ExceptionHandler(SQLIntegrityConstraintViolationException.class)
+  public ResponseEntity<Object> handleSQLIntegrityConstraintViolationException(@Nonnull SQLIntegrityConstraintViolationException ex,
+                                                                               @Nonnull HttpHeaders headers,
+                                                                               @Nonnull WebRequest request) {
+    return new ResponseEntity<>(new ErrorJson(
+        new Date(),
+        HttpStatus.BAD_REQUEST.value(),
+        List.of("Объект не может быть сохранен по причине дублирования первичного ключа")
+    ), headers, HttpStatus.BAD_REQUEST);
+  }
+}
