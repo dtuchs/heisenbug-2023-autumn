1bd293753082e8755b32901929acf58949a10dc6
dtuchs <dtuchs@gmail.com>
2023-10-04 04:52:42 +0600

    remove kafka

--- rococo-api/src/main/java/guru/qa/rococo/service/UserService.java
+++ rococo-api/src/main/java/guru/qa/rococo/service/UserService.java
@@ -6,19 +6,15 @@ import guru.qa.rococo.exception.NotFoundException;
 import guru.qa.rococo.model.UserJson;
 import guru.qa.rococo.model.util.StringAsBytes;
 import jakarta.annotation.Nonnull;
-import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.UUID;
-import java.util.function.Supplier;
 
 @Service
 public class UserService {
@@ -69,13 +65,6 @@ public class UserService {
     return UserJson.fromEntity(
         userRepository.save(userEntity)
     );
-  }
-
-  @Transactional
-  @KafkaListener(topics = TOPIC, groupId = "rococo-api")
-  public void listener(@Payload UserJson userJson, ConsumerRecord<String, UserJson> cr) {
-    LOG.info("### Kafka consumer record: " + cr.toString());
-    userRepository.save(userJson.toEntity());
   }
 
   private @Nonnull UserEntity getRequiredUser(@Nonnull String username) {
