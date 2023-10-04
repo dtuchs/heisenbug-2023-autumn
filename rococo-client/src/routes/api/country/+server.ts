a80b13111a7492689f3c684a115b114d6e3f63ce
IrinaStyazhkina <irastyazhkina@gmail.com>
2023-10-04 14:29:45 +0300

    remove mocks

--- rococo-client/src/routes/api/country/+server.ts
+++
@@ -1,9 +0,0 @@
-import { json } from '@sveltejs/kit';
-
-import type { RequestHandler } from './$types';
-import { countries } from '../../../mock/counties';
-
-export const GET: RequestHandler = async ({ request }) => {
-
-    return json(countries);
-};
