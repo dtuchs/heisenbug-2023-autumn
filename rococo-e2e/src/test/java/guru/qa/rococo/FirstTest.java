43a04fd751f2ba0eb2a5a29cf7831bedcee94485
dtuchs <dtuchs@gmail.com>
2023-10-05 21:47:04 +0600

    prepare to test

---
+++ rococo-e2e/src/test/java/guru/qa/rococo/FirstTest.java
@@ -0,0 +1,13 @@
+package guru.qa.rococo;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class FirstTest {
+
+  @Test
+  void firstTest() {
+    assertTrue(true);
+  }
+}
