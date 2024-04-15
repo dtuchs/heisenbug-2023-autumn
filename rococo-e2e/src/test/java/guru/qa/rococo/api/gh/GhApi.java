c5e3ec876cf78b629a433bc6326a0049401dcf84
dtuchs <dtuchs@gmail.com>
2024-04-15 20:24:04 +0500

    prepare to HB vol 2.2

--- rococo-e2e/src/test/java/guru/qa/rococo/api/gh/GhApi.java
+++ rococo-e2e/src/test/java/guru/qa/rococo/api/gh/GhApi.java
@@ -8,7 +8,7 @@ import retrofit2.http.Headers;
 import retrofit2.http.Path;
 
 public interface GhApi {
-  @GET("repos/qa-guru/niffler/issues/{ISSUE_NUMBER}")
+  @GET("repos/dtuchs/heisenbug-2023-autumn/issues/{ISSUE_NUMBER}")
   @Headers({
       "Accept: application/vnd.github+json",
       "X-GitHub-Api-Version: 2022-11-28"
