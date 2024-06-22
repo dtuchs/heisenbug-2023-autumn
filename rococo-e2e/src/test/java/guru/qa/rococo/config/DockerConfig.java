abfb24b3da7a873ca0cfd15e809de3658206b8c6
dtuchs <dtuchs@gmail.com>
2024-06-23 01:25:22 +0600

    fix

--- rococo-e2e/src/test/java/guru/qa/rococo/config/DockerConfig.java
+++ rococo-e2e/src/test/java/guru/qa/rococo/config/DockerConfig.java
@@ -1,10 +1,22 @@
 package guru.qa.rococo.config;
 
+import com.codeborne.selenide.Configuration;
+import org.openqa.selenium.chrome.ChromeOptions;
+
 public class DockerConfig implements Config {
 
   static final DockerConfig INSTANCE = new DockerConfig();
 
   private DockerConfig() {
+  }
+
+  static {
+    Configuration.browserSize = "1920x1200";
+    Configuration.remote = "http://selenoid:4444/wd/hub";
+    Configuration.timeout = 10000;
+    Configuration.browser = "chrome";
+    Configuration.browserVersion = "125.0";
+    Configuration.browserCapabilities = new ChromeOptions().addArguments("--no-sandbox");
   }
 
   @Override
